e548fdbeba1937e0f7c656de86d932885157b2d2
Katarina Mitic <kmitic764@gmail.com>
2023-08-24 16:28:54 +0200

    Napravljen Navbar(css,js)

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo'>
+            STUDENTSKI DOMOVI
+            <i class="fas fa-dom"></i>
+          </Link>
+          <ul className='nav-menu'>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links'>
+                Početna
+              </Link>
+            </li>
+            <li>
+              <Link
+                to='/appointments'
+                className='nav-links'>
+                Lokacije
+              </Link>
+            </li> 
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
