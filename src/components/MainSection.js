c908dfbaaad414be05c333d97ab73afca3b9c132
Katarina Mitic <kmitic764@gmail.com>
2023-08-24 16:17:00 +0200

    Napravljen MainSection(css,js) i dodat na App.js

---
+++ src/components/MainSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import './MainSection.css';
+
+function MainSection() {
+  return (
+    <div className='main-container'>
+      <image src='../assets/bckg.jpg'/>
+      <h1>STUDENTSKI DOMOVI</h1>
+      <p>Mesta gde se prave najlepše uspomene!</p>
+    </div>
+  );
+}
+
+export default MainSection;
